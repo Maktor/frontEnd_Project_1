3abc5703305643f6249c50bf99a3c9fda4073005
Maktor <66954908+Maktor@users.noreply.github.com>
2023-02-02 20:03:56 -0500

    new game :D

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const placy = document.getElementById("placy");
+const guessInput = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessButton");
+const newWord = document.getElementById("newWord");
+const incorrectGuesses = document.getElementById("incorrectGuesses");
