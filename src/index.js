3566551d951bccf15711521d1b2e5e1185e871f8
Maktor <66954908+Maktor@users.noreply.github.com>
2023-02-02 20:04:38 -0500

    new game :D

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ const guessInput = document.getElementById("guessInput");
 const guessButton = document.getElementById("guessButton");
 const newWord = document.getElementById("newWord");
 const incorrectGuesses = document.getElementById("incorrectGuesses");
+
+const words = ["website", "javascript", "mobile", "laptop", "programming", "coding", "aaron", "macbook", "library", "women", "men"]
+var word = words[Math.floor(Math.random() * words.length)];
+
