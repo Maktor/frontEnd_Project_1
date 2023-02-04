d121527c1a8654c00a7a2f6a44ff43185ffd2055
Maktor <66954908+Maktor@users.noreply.github.com>
2023-02-03 20:44:45 -0500

    updated

---
+++ 2p/src/script.js
@@ -0,0 +1,61 @@
+const mainDiv = document.getElementById("main");
+
+let createOnce = 1;
+storeWord = ""
+
+let ptag = document.createElement("p");
+ptag.innerHTML = "Player 2, close your eyes! Player 1, click next.";
+mainDiv.appendChild(ptag);
+
+let nextButton1 = document.createElement("button");
+nextButton1.innerHTML = "Next";
+mainDiv.appendChild(nextButton1);
+nextButton1.id = "nextButton";
+
+nextButton1.addEventListener("click", () => {
+
+  if(createOnce == 1){
+    createOnce -= 1;
+    nextButton1.remove()
+
+    let nextButton2 = document.createElement("button");
+    nextButton2.innerHTML = "Next";
+    mainDiv.appendChild(nextButton2);
+    nextButton2.id = "nextButton";  
+  
+    ptag.innerHTML = "Player 1, type in one word that you want Player 2 to guess. Click next to continue.";
+  
+    let guessWordInput = document.createElement("input");
+    mainDiv.appendChild(guessWordInput);
+    guessWordInput.id = "guessInput";
+  
+    nextButton2.addEventListener("click", () => {
+      if(guessWordInput.value.length === 0){
+        alert("Input is empty, type in a word!");
+      } else {
+        nextButton2.remove();
+        guessWordInput.remove();
+
+        storeWord = guessWordInput.value;
+        alert(guessWordInput.value);
+
+        ptag.innerHTML = "Player 2, start guessing letters. You only have 5 shots!";
+
+        let nextButton3 = document.createElement("button");
+        nextButton3.innerHTML = "Next";
+        mainDiv.appendChild(nextButton3);
+        nextButton3.id = "nextButton";
+
+        const placy = document.getElementById("placy");
+        const guessInput = document.getElementById("guessInput");
+        const guessButton = document.getElementById("guessButton");
+        const newWord = document.getElementById("newWord");
+        const incorrectGuesses = document.getElementById("incorrectGuesses");
+
+        
+
+
+      }
+    });
+  }
+});
